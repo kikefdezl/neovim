3d8d794470bd8b6095267090b4f056f665d2ad6d
kike <enriquelagui@outlook.com>
2025-03-19 12:17:04 +0100

    Enable rust-analyzer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -154,7 +154,7 @@ return {
     local servers = {
       -- clangd = {},
       -- gopls = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
