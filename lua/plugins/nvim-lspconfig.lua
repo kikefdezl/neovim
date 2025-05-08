dedf036df658cb0a6251d114ab92fa1425eb331c
kike <enriquelagui@outlook.com>
2025-05-08 11:16:56 +0200

    Replace pyright LSP with basedpyright LSP

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -153,15 +153,16 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`ts_ls`) will work just fine
-      -- ts_ls = {},
 
       -- python
-      pyright = {},
+      basedpyright = {
+        capabilities = capabilities,
+        settings = {
+          basedpyright = {
+            typeCheckingMode = "standard",
+          },
+        },
+      },
 
       -- rust
       rust_analyzer = {},
@@ -174,16 +175,11 @@ return {
 
       -- lua
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
               callSnippet = "Replace",
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
