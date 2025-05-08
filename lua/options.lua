f516c2975483b9b2f9df21b3c915dcffdbff430b
kike <enriquelagui@outlook.com>
2025-05-08 11:16:18 +0200

    Enable in-line diagnostics

--- lua/options.lua
+++ lua/options.lua
@@ -57,3 +57,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Show LSP diagnostics in-line
+vim.diagnostic.config {
+  virtual_text = true,
+}
