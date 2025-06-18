cc6c5223ecbe7469769c593ac9af42ad5834da45
kike <enriquelagui@outlook.com>
2025-06-18 11:03:08 +0200

    Update lsp config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -126,13 +126,13 @@ return {
 
       -- python
       basedpyright = {
-        capabilities = capabilities,
         settings = {
           basedpyright = {
-            typeCheckingMode = "standard",
+            typeCheckingMode = "basic",
           },
         },
       },
+      ruff = {},
 
       -- rust
       rust_analyzer = {},
@@ -154,8 +154,11 @@ return {
         },
       },
 
+      -- json
+      jq = {},
+
       -- yaml
-      -- yamlls = {},
+      yamlls = {},
       helm_ls = {},
     }
 
@@ -170,12 +173,12 @@ return {
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      "stylua", -- Used to format Lua code
-    })
+    vim.list_extend(ensure_installed, {}) -- add other stuff to keep installed here
     require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
     require("mason-lspconfig").setup {
+      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer above)
+      automatic_enable = false,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
