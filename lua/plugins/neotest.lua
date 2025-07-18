7a97b79b68880a33b23a1145e49dbb3a2e58dda5
kike <enriquelagui@outlook.com>
2025-07-18 13:14:12 +0200

    Add shortcut for neotest output panel open:

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -26,16 +26,20 @@ return {
     vim.keymap.set("n", "<leader>tr", function()
       neotest.run.run()
       neotest.output.open()
-    end, { desc = "[r]un the nearest [t]test" })
+    end, { desc = "[r]un the nearest [t]est" })
 
     vim.keymap.set("n", "<leader>td", function()
       neotest.run.run { strategy = "dap" }
-    end, { desc = "[d]ebug the nearest [t]test" })
+    end, { desc = "[d]ebug the nearest [t]est" })
 
     vim.keymap.set("n", "<leader>tf", function()
       neotest.run.run(vim.fn.expand "%")
     end, { desc = "[T]est the entire [f]ile" })
 
     vim.keymap.set("n", "<leader>ts", neotest.summary.toggle, { desc = "[T]est [s]ummary" })
+
+    vim.keymap.set("n", "<leader>to", function()
+      neotest.output.open()
+    end, { desc = "[o]pen the [t]est output panel" })
   end,
 }
