4db2d800e43d9babfbe46c5df4bb654828813915
kike <enriquelagui@outlook.com>
2025-08-05 22:12:28 +0200

    Fix DAP for rust

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -1,33 +1,24 @@
 -- NOTE: rustaceanvim sets up LSP for us, so don't configure it
--- via nvim-lspconfig as it may cause conflicts
+-- via nvim-lspconfig as it may cause conflicts.
+-- Also, it just happens to magically interface with nvim-dap as
+-- long as codelldb is installed.
+
 return {
   "mrcjkb/rustaceanvim",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+  },
   version = "^6",
   lazy = false, -- This plugin is already lazy
   ft = "rust",
 
-  -- TODO: requires `rust-analyzer` and `codelldb`, ensure installed with Mason
+  -- NOTE: Requires `rust_analyzer` but the rustup installed one is enough
+  -- so don't install via Mason or you get double diagnostics
 
   config = function()
-    local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
-    local codelldb_path = extension_path .. "adapter/codelldb"
-    local liblldb_path = extension_path .. "lldb/lib/liblldb"
-    local this_os = vim.uv.os_uname().sysname
-
-    -- The path is different on Windows
-    if this_os:find "Windows" then
-      codelldb_path = extension_path .. "adapter\\codelldb.exe"
-      liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-    else
-      -- The liblldb extension is .so for Linux and .dylib for MacOS
-      liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
-    end
-
-    local cfg = require "rustaceanvim.config"
     return {
-      dap = {
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-      },
+      -- TODO: requires `codelldb`, ensure installed with Mason
+      dap = {},
     }
   end,
 }
