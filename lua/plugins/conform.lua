8ca7c42e5a26affda5eacf416623e6d8d09be9c9
kike <enriquelagui@outlook.com>
2025-11-22 18:58:28 +0100

    Add go formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,13 +14,14 @@ return {
   opts = {
     notify_on_error = true,
     formatters_by_ft = {
+      go = { "gofumpt", "goimports" },
+      json = { "jq" },
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
       lua = { "stylua" },
       python = { "ruff_organize_imports", "ruff_format" },
       rust = { "rustfmt" },
       terraform = { "terraform_fmt" },
-      json = { "jq" },
-      javascript = { "prettierd" },
-      javascriptreact = { "prettierd" },
       typescript = { "prettierd" },
       typescriptreact = { "prettierd" },
     },
