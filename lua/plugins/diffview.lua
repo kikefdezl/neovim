18f8f066a5b100c9dcad40b271201239d1f73af0
Kike <enriquelagui@outlook.com>
2026-01-14 19:58:32 +0100

    Set cool diagonal diff for diffview

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,5 +5,11 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>go", ":DiffviewOpen<CR>")
     vim.keymap.set("n", "<leader>gq", ":DiffviewClose<CR>")
+
+    vim.opt.fillchars = vim.opt.fillchars + "diff:╱"
+
+    require("diffview").setup {
+      enhanced_diff_hl = true,
+    }
   end,
 }
