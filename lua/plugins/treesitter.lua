576253269e2ef69b04e7ccbd5f90edb22553b5d8
kike <enriquelagui@outlook.com>
2026-01-17 16:13:57 +0100

    fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,12 @@
--- NOTE: With the new "main" default branch, you need to have tree-sitter-cli 
--- installed in the system, or you get a "nvim-treesitter/compiling/language" message 
--- on every startup.
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter").install {
       "bash",
       "c",
-      "diff",
       "dockerfile",
       "go",
       "javascript",
@@ -19,32 +16,6 @@ return {
       "markdown",
       "python",
       "rust",
-      "vim",
-      "vimdoc",
     }
   end,
-  -- main = "nvim-treesitter.configs",
-  -- opts = {
-  --   ensure_installed = {
-  --     "bash",
-  --     "c",
-  --     "diff",
-  --     "dockerfile",
-  --     "html",
-  --     "lua",
-  --     "luadoc",
-  --     "markdown",
-  --     "markdown_inline",
-  --     "python",
-  --     "rust",
-  --     "terraform",
-  --     "query",
-  --     "vim",
-  --     "vimdoc",
-  --   },
-  --   auto_install = true,
-  --   highlight = {
-  --     enable = true,
-  --   },
-  -- },
 }
