9d79b87b05100909cdc739d4dc1606bc467d3587
Kike <enriquelagui@outlook.com>
2026-01-29 14:24:50 +0100

    Reword telescope shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,34 +20,21 @@ return {
     -- See `:help telescope.builtin`
     local builtin = require "telescope.builtin"
 
-    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+    -- find files
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[S]earch [F]iles" })
-
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "[F]ind [R]ecent Files" })
     vim.keymap.set("n", "<leader>fh", function()
       builtin.find_files { hidden = true, no_ignore = true }
     end, { desc = "[F]ind [H]idden Files" })
 
-    vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+    -- search
+    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind by [G]rep" })
-    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+    vim.keymap.set("n", "<leader>sx", builtin.diagnostics, { desc = "[S]earch Diagnosti[x]" })
     vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-
-    vim.keymap.set("n", "<leader>sn", function()
-      builtin.find_files { cwd = vim.fn.stdpath "config" }
-    end, { desc = "[S]earch [N]eovim files" })
-
-    vim.keymap.set("n", "<leader>fo", function()
-      require("lazy").load { plugins = { "obsidian.nvim" } }
-      vim.cmd "ObsidianQuickSwitch"
-    end, { desc = "Obsidian search" })
-    vim.keymap.set("n", "<leader>so", function()
-      require("lazy").load { plugins = { "obsidian.nvim" } }
-      vim.cmd "ObsidianSearch"
-    end, { desc = "Obsidian search" })
 
     vim.keymap.set("n", "<leader>s/", function()
       builtin.live_grep {
@@ -55,5 +42,28 @@ return {
         prompt_title = "Live Grep in Open Files",
       }
     end, { desc = "[S]earch [/] in Open Files" })
+
+    -- Neovim
+    vim.keymap.set("n", "<leader>sn", function()
+      builtin.find_files { cwd = vim.fn.stdpath "config" }
+    end, { desc = "[S]earch [N]eovim files" })
+
+    -- Dotfiles
+    vim.keymap.set("n", "<leader>fd", function()
+      builtin.find_files {
+        cwd = "~/.dotfiles",
+        hidden = true,
+      }
+    end, { desc = "[F]ind [D]otfiles" })
+
+    -- Obsidian
+    vim.keymap.set("n", "<leader>fo", function()
+      require("lazy").load { plugins = { "obsidian.nvim" } }
+      vim.cmd "ObsidianQuickSwitch"
+    end, { desc = "[F]ind [O]bsidian file" })
+    vim.keymap.set("n", "<leader>so", function()
+      require("lazy").load { plugins = { "obsidian.nvim" } }
+      vim.cmd "ObsidianSearch"
+    end, { desc = "[S]earch [O]bsidian (grep)" })
   end,
 }
