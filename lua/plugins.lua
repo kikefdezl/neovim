1118f6ea5381bdc575c6b377424bcb8cfc77ee78
kike <enriquelagui@outlook.com>
2026-02-05 12:31:37 +0100

    Make indent-blanline more minimal, remove custom mltop plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -55,5 +55,3 @@ require("lazy").setup({
     },
   },
 })
-
-require "custom.mltop"
--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,8 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
-  config = function()
-    local hl = vim.api.nvim_get_hl_by_name("Keyword", true)
-    vim.api.nvim_set_hl(0, "hl-IblScope", { fg = hl.foreground })
-
-    require("ibl").setup {
-      indent = { char = "▏" },
-      scope = {
-        enabled = true,
-        show_start = false,
-        show_end = false,
-        highlight = "hl-IblScope",
-      },
-    }
-  end,
+  opts = {
+    indent = { char = "▏" },  -- thinner than default
+    scope = { enabled = false },
+  },
 }
