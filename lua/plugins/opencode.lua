3203d40efa70da7cd1a7c55320014e9fc0e5b794
kike <enriquelagui@outlook.com>
2026-02-09 15:36:24 +0100

    Add system notification when opencode finishes

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -12,6 +12,7 @@ return {
 
     vim.o.autoread = true
 
+    -- Your existing keymaps
     vim.keymap.set({ "n", "x" }, "<A-a>", function()
       require("opencode").ask("@this: ", { submit = true })
     end, { desc = "[A]sk opencode" })
@@ -21,19 +22,45 @@ return {
     vim.keymap.set({ "n", "t" }, "<A-o>", function()
       require("opencode").toggle()
     end, { desc = "Toggle [o]pencode" })
-
     vim.keymap.set({ "n", "x" }, "go", function()
       return require("opencode").operator "@this "
     end, { expr = true, desc = "Add range to opencode" })
     vim.keymap.set("n", "goo", function()
       return require("opencode").operator "@this " .. "_"
     end, { expr = true, desc = "Add line to opencode" })
-
     vim.keymap.set("n", "<S-C-u>", function()
       require("opencode").command "session.half.page.up"
     end, { desc = "opencode half page up" })
     vim.keymap.set("n", "<S-C-d>", function()
       require("opencode").command "session.half.page.down"
     end, { desc = "opencode half page down" })
+
+    -- Notification on Opencode finishing
+    vim.api.nvim_create_autocmd("User", {
+      pattern = { "OpencodeEvent", "OpencodeEvent:*" },
+      callback = function(args)
+        local event = args.data and args.data.event
+        if not event then
+          return
+        end
+
+        -- session.idle is the event type that indicates opencode has finished responding
+        if event.type == "session.idle" then
+          vim.notify("Opencode session is idle", vim.log.levels.INFO, { title = "opencode" })
+
+          if vim.fn.executable "notify-send" == 1 then
+            vim.fn.jobstart({
+              "notify-send",
+              "-a",
+              "neovim-opencode",
+              "-u",
+              "low",
+              "Opencode Done!",
+            }, { detach = true })
+          end
+        end
+      end,
+      desc = "Send system notification when Opencode finishes processing",
+    })
   end,
 }
