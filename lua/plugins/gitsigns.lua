a6edee67ed2dbee6715f675526ddf76e3f4fafdf
Kike <enriquelagui@outlook.com>
2026-02-19 11:09:13 +0100

    feat: shortcuts for staging/unstaging/restoring git hunks

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,5 +8,12 @@ return {
       topdelete = { text = "‾" },
       changedelete = { text = "~" },
     },
+    on_attach = function(bufnr)
+      local gitsigns = require "gitsigns"
+
+      vim.keymap.set({ "n", "v" }, "<leader>ga", gitsigns.stage_hunk, { desc = "[G]it [A]ccept hunk (stages it)" })
+      vim.keymap.set({ "n", "v" }, "<leader>gX", gitsigns.reset_hunk, { desc = "Rejet hunk (restores it)" })
+      vim.keymap.set("n", "<leader>gb", gitsigns.blame_line, { desc = "git [b]lame line" })
+    end,
   },
 }
